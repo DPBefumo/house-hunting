255fb4269c58a873459a6df2c5c030a40bd641cb
Danny Befumo <DPBefumo@gmail.com>
2020-05-18 14:56:02 -0400

    1-7 complete  need to write program

--- house_hunting.py
+++ house_hunting.py
@@ -15,25 +15,28 @@ print("Total cost of your dream is: ", total_cost)
 portion_down_payment = float(total_cost)*.25
 print("Your down payment will need to be: ", portion_down_payment)
 
-# 3
+# 3git 
 current_savings = input("Current savings in the bank:")
 print("Your savings for your house is: ", current_savings)
 
 # 4
 r = 0.04
-increase_monthly_savings = int(current_savings)*r/12
-print("You will increase your savings monthly by: ", increase_monthly_savings)
+return_on_investment = int(current_savings)*r/12
+print("You will increase your savings monthly by: ", return_on_investment)
 
-# 5. Call your annual salary `annual_salary​`.
+# 5
 annual_salary = input("Current annual salary:")
 print("Your annual salary is: ", annual_salary)
 
 
-# 6. Assume you are going to dedicate a certain amount of your salary each month to saving for the down payment. `Call that portion_saved`​. This variable should be in decimal form (i.e. 0.1 for 10%).
+# 6
+salary_saved = int(annual_salary)*.15/12
+print("Amount saved each month from salary: ", salary_saved)
 
 
 # 7. At the end of each month, your savings will be increased by the return on your investment, plus a percentage of your monthly salary ​(annual salary / 12).
-
+increase_monthly_savings = salary_saved+return_on_investment
+print("Each month you are saving: ", increase_monthly_savings)
 
 # Write a program to calculate how many months it will take you to save up enough money for a down payment.
     # Your program should ask the user to enter the following variables:
